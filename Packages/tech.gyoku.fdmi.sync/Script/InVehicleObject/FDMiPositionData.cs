2d09ab096ac51dc066df271f4eecfedba0d36625
ぎょくて <gyokutech@gmail.com>
2023-09-11 01:45:47 +0900

    位置情報のパッケージ化
    ・グローバル位置・速度・姿勢をFDMiDataに変換するスクリプト

---
+++ Packages/tech.gyoku.fdmi.sync/Script/InVehicleObject/FDMiPositionData.cs
@@ -0,0 +1,36 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using tech.gyoku.FDMi.core;
+
+namespace tech.gyoku.FDMi.sync
+{
+    public class FDMiPositionData : FDMiAttribute
+    {
+        public FDMiReferencePoint refPoint;
+        public FDMiVector3 Position, KmPosition, Velocity, Attitude;
+        public FDMiQuaternion Rotation;
+        Vector3[] pos, kmPos, vel, attitude;
+        Quaternion[] rot;
+        void Start()
+        {
+            pos = Position.data;
+            kmPos = KmPosition.data;
+            vel = Velocity.data;
+            attitude = Attitude.data;
+            rot = Rotation.data;
+        }
+
+        void FixedUpdate()
+        {
+            pos[0] = refPoint._position;
+            kmPos[0] = refPoint._kmPosition;
+            vel[0] = refPoint.velocity;
+            rot[0] = refPoint._direction * body.rotation;
+            attitude[0] = rot[0].eulerAngles;
+
+        }
+    }
+}
