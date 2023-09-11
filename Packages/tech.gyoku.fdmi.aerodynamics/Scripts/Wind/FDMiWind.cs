3dc90d5f687094a5b5395b53c9c8bb3d99c28a92
ぎょくて <gyokutech@gmail.com>
2023-09-11 23:56:05 +0900

    FDMiWind
    * 風。
      * ”Sacc互換ではない”

---
+++ Packages/tech.gyoku.fdmi.aerodynamics/Scripts/Wind/FDMiWind.cs
@@ -0,0 +1,37 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using tech.gyoku.FDMi.core;
+
+namespace tech.gyoku.FDMi.aerodynamics
+{
+    public class FDMiWind : FDMiAttribute
+    {
+        public FDMiVector3 Wind, Position;
+        public FDMiSyncedVector3 WindConstant;
+        public FDMiSyncedFloat WindTurbulanceScale, WindGustStrength;
+        private Vector3[] pos, wind, windConst;
+        private float[] turbulanceScale, gustStrength;
+
+        void Start()
+        {
+            pos = Position.data;
+            wind = Wind.data;
+            windConst = WindConstant.data;
+            turbulanceScale = WindTurbulanceScale.data;
+            gustStrength = WindGustStrength.data;
+        }
+        void Update()
+        {
+            // idea from Saccflight.
+            Vector3 gust = Time.time * windConst[0] + turbulanceScale[0] * pos[0];
+            gust.x = Mathf.PerlinNoise(gust.x, 0f);
+            gust.y = Mathf.PerlinNoise(gust.y, 0.3f);
+            gust.z = Mathf.PerlinNoise(gust.z, 0.6f);
+            Vector3 FinalWind = Vector3.Normalize(gust - 0.5f * Vector3.one) * gustStrength[0];
+            wind[0] = (FinalWind + windConst[0]);
+        }
+    }
+}
