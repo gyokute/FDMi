1bf34b355f9eab990c21b43ae9b575574973db29
ぎょくて <gyokutech@gmail.com>
2023-09-13 00:38:27 +0900

    FDMiObjectActivator
    ・乗り降り/パイロット判定でオブジェクトをOn/Offするように

---
+++ Packages/tech.gyoku.fdmi.core/Script/Object/FDMiObjectActivator.cs
@@ -0,0 +1,23 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+namespace tech.gyoku.FDMi.core
+{
+    public class FDMiObjectActivator : FDMiAttribute
+    {
+        public FDMiBool InZone, IsPilot;
+        public bool enableInZone, enableIsPilot;
+        void Start()
+        {
+            InZone.subscribe(this, "OnChangeInZone");
+            gameObject.SetActive(false);
+        }
+        public void OnChangeInZone()
+        {
+            if (enableInZone) gameObject.SetActive(InZone.data[0]);
+        }
+        public void OnChangeIsPilot() { }
+    }
+}
