08d259a7701bcf6631c441a706e493dd4dd16ba4
ぎょくて <gyokutech@gmail.com>
2023-09-17 20:11:29 +0900

    wing actuator
    ・角度制御
    ・翼幅制御

---
+++ Packages/tech.gyoku.fdmi.aerodynamics/Scripts/Actuator/FDMiWingAngleActuator.cs
@@ -0,0 +1,31 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using tech.gyoku.FDMi.core;
+
+namespace tech.gyoku.FDMi.aerodynamics
+{
+    public class FDMiWingAngleActuator : UdonSharpBehaviour
+    {
+        public FDMiFloat Angle;
+        public FDMiWing wing;
+        public Vector3 rotateAxis;
+        private float[] angle;
+        private float initialAngle;
+        private Transform wingTransform;
+        private Vector3 initialRotation;
+        void Start()
+        {
+            angle = Angle.data;
+            initialAngle = angle[0];
+            wingTransform = wing.transform;
+            initialRotation = wingTransform.localEulerAngles - angle[0] * rotateAxis;
+        }
+        void LateUpdate()
+        {
+            wingTransform.localEulerAngles = initialRotation + angle[0] * rotateAxis;
+        }
+    }
+}
---
+++ Packages/tech.gyoku.fdmi.aerodynamics/Scripts/Actuator/FDMiWingExtendActuator.cs
@@ -0,0 +1,27 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using tech.gyoku.FDMi.core;
+namespace tech.gyoku.FDMi.aerodynamics
+{
+    public class FDMiWingExtendActuator : UdonSharpBehaviour
+    {
+        public FDMiFloat Angle;
+        public FDMiWing wing;
+        public AnimationCurve multiply;
+        private float[] angle;
+        private float initialAngle;
+        private Transform wingTransform;
+        void Start()
+        {
+            angle = Angle.data;
+            initialAngle = angle[0];
+        }
+        void LateUpdate()
+        {
+            wing.cpChordLength = multiply.Evaluate(angle[0] - initialAngle);
+        }
+    }
+}
