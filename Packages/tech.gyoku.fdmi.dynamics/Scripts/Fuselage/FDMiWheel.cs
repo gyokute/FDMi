f6b5c99ef5d923678a09b8c1ca0fdec68bc97b27
ぎょくて <gyokutech@gmail.com>
2023-09-17 22:41:12 +0900

    FDMiWheel

---
+++ Packages/tech.gyoku.fdmi.dynamics/Scripts/Fuselage/FDMiWheel.cs
@@ -0,0 +1,34 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using tech.gyoku.FDMi.core;
+
+namespace tech.gyoku.FDMi.dynamics
+{
+    public class FDMiWheel : FDMiAttribute
+    {
+        public WheelCollider wheel;
+        public FDMiFloat TillerInput, BrakeInput, ParkBrakeInput;
+        public float brakePressure = 3000f, parkBrakePressure = 3000f;
+        [SerializeField] private float preLoadTorque = 0.0001f;
+        [SerializeField] private float rpm;
+        [SerializeField] private float rotateAngle = 60f;
+        private float[] tiller, brake, parkbrake;
+        void Start()
+        {
+            tiller = TillerInput.data;
+            brake = BrakeInput.data;
+            parkbrake = ParkBrakeInput.data;
+        }
+        void Update()
+        {
+            rpm = wheel.rpm;
+            // Ground Detection
+            wheel.brakeTorque = parkbrake[0] * brakePressure + parkbrake[0] * parkBrakePressure;
+            wheel.motorTorque = Mathf.Clamp01(wheel.rpm + 0.01f) * Mathf.Clamp01(1f - parkbrake[0] - parkbrake[0]) * preLoadTorque;
+            wheel.steerAngle = tiller[0] * rotateAngle;
+        }
+    }
+}
