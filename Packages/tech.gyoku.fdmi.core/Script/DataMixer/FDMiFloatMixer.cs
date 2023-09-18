1f66c4306ecb49b1de7fbd8e61c63617eb630c6a
ぎょくて <gyokutech@gmail.com>
2023-09-18 20:14:54 +0900

    DataMixer
    ・FDMiSyncedValueをFDMiFloatに変換したりできる

---
+++ Packages/tech.gyoku.fdmi.core/Script/DataMixer/FDMiFloatMixer.cs
@@ -0,0 +1,24 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace tech.gyoku.FDMi.core
+{
+    public class FDMiFloatMixer : UdonSharpBehaviour
+    {
+        public FDMiFloat[] output;
+        public FDMiFloat[] data;
+        public FDMiSyncedFloat[] syncedData;
+        public float multiply, min, max;
+
+        void Update()
+        {
+            float outs = 0f;
+            foreach (FDMiFloat d in data) outs += d.data[0];
+            foreach (FDMiSyncedFloat d in syncedData) outs += d.data[0];
+            foreach (FDMiFloat d in output) d.Data = Mathf.Clamp(outs * multiply, min, max);
+        }
+    }
+}
---
+++ Packages/tech.gyoku.fdmi.core/Script/DataMixer/FDMiFloataccumulator.cs
@@ -0,0 +1,24 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace tech.gyoku.FDMi.core
+{
+    public class FDMiFloataccumulator : UdonSharpBehaviour
+    {
+        public FDMiFloat[] output;
+        public FDMiFloat[] data;
+        public FDMiSyncedFloat[] syncedData;
+        public float multiply, min, max;
+
+        void Update()
+        {
+            float outs = 0f;
+            foreach (FDMiFloat d in data) outs += d.data[0];
+            foreach (FDMiSyncedFloat d in syncedData) outs += d.data[0];
+            foreach (FDMiFloat d in output) d.Data = Mathf.Clamp(d.data[0] + outs * multiply * Time.deltaTime, min, max);
+        }
+    }
+}
