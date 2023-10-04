f4283035ffb7a1093f1c2ffc114ccb2e3806f2f7
ぎょくて <gyokutech@gmail.com>
2023-10-05 00:14:19 +0900

    AnimationDriver
    ・Animationを動かすもの

---
+++ Packages/tech.gyoku.fdmi.core/Script/Animation/FDMiAnimatorDriver.cs
@@ -0,0 +1,28 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+namespace tech.gyoku.FDMi.core
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class FDMiAnimatorDriver : UdonSharpBehaviour
+    {
+        [SerializeField] private FDMiFloat Input;
+        private float[] input = new float[1];
+        [SerializeField] private Animator animator;
+        [SerializeField] private string paramator;
+        [SerializeField] private AnimationCurve outputValue;
+        public
+        void Start()
+        {
+            input = Input.data;
+        }
+
+        void Update()
+        {
+            animator.SetFloat(paramator, outputValue.Evaluate(input[0]));
+        }
+
+    }
+}
---
+++ Packages/tech.gyoku.fdmi.core/Script/Animation/FDMiTransformDriver.cs
@@ -0,0 +1,28 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+namespace tech.gyoku.FDMi.core
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class FDMiTransformDriver : UdonSharpBehaviour
+    {
+        [SerializeField] private FDMiFloat Input;
+        private float[] input = new float[1];
+        [SerializeField] private Transform targetTransform;
+        [SerializeField] private Vector3 zeroPosition, onePosition;
+        [SerializeField] private AnimationCurve outputValue;
+        public
+        void Start()
+        {
+            input = Input.data;
+        }
+
+        void Update()
+        {
+            targetTransform.localPosition = Vector3.Lerp(zeroPosition, onePosition, outputValue.Evaluate(input[0]));
+        }
+
+    }
+}
