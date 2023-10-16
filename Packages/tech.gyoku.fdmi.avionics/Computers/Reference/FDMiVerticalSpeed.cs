c590845c35ca639b328dfb41400b94f371ce07b0
ぎょくて <gyokutech@gmail.com>
2023-10-17 00:47:48 +0900

    FDMiVerticalSpeed
    ・昇降計

---
+++ Packages/tech.gyoku.fdmi.avionics/Computers/Reference/FDMiVerticalSpeed.cs
@@ -0,0 +1,26 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using tech.gyoku.FDMi.core;
+
+namespace tech.gyoku.FDMi.avionics
+{
+    public class FDMiVerticalSpeed : FDMiAttribute
+    {
+        [SerializeField] FDMiFloat VerticalSpeed;
+        [SerializeField] FDMiVector3 Velocity;
+        float[] vs;
+        Vector3[] vel;
+        void Start()
+        {
+            vs = VerticalSpeed.data;
+            vel = Velocity.data;
+        }
+        void Update()
+        {
+            vs[0] = vel[0].y;
+        }
+    }
+}
