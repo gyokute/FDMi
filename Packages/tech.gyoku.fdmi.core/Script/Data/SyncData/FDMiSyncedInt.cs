0df610f21f64900f4ce5dad99e9ebbae43a1ae8d
ぎょくて <gyokutech@gmail.com>
2023-10-23 22:37:36 +0900

    FDMiInt
    ・FDMiDataにFDMiIntを追加

---
+++ Packages/tech.gyoku.fdmi.core/Script/Data/SyncData/FDMiSyncedInt.cs
@@ -0,0 +1,31 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace tech.gyoku.FDMi.core
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class FDMiSyncedInt : FDMiInt
+    {
+        [UdonSynced, FieldChangeCallback(nameof(SyncedData)), HideInInspector] public int syncedData;
+        public int SyncedData
+        {
+            get => syncedData;
+            set
+            {
+                syncedData = value;
+                data[0] = value;
+                trigger();
+            }
+        }
+
+        public override void set(int src)
+        {
+            SyncedData = src;
+            if (!Networking.IsOwner(gameObject)) Networking.SetOwner(Networking.LocalPlayer, gameObject);
+            RequestSerialization();
+        }
+    }
+}
