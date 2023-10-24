0d36141dc4f624fcd3066c8cadff77afff2bfe1e
ぎょくて <gyokutech@gmail.com>
2023-10-25 01:09:39 +0900

    FDMiInputButton
    ・Bool用

---
+++ Packages/tech.gyoku.fdmi.input/Script/InputAddon/Addons/FDMiButtonInput.cs
@@ -0,0 +1,33 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using tech.gyoku.FDMi.core;
+
+namespace tech.gyoku.FDMi.input
+{
+    public class FDMiButtonInput : FDMiInputAddon
+    {
+        [SerializeField] FDMiSyncedBool Output;
+        [SerializeField] AxisBehaviourType behaviourType;
+        protected override void Update()
+        {
+            base.Update();
+            if (behaviourType == AxisBehaviourType.momentum) Output.set(true);
+        }
+        public override void OnCalled(KeyCode callKey, VRCPlayerApi.TrackingDataType trackType)
+        {
+            base.OnCalled(callKey, trackType);
+            if (behaviourType == AxisBehaviourType.alternate)
+            {
+                Output.set(!Output.Data);
+            }
+        }
+        public override void OnReleased()
+        {
+            base.OnReleased();
+            if (behaviourType == AxisBehaviourType.momentum) Output.set(false);
+        }
+    }
+}
