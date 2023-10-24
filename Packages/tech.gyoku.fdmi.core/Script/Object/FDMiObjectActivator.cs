e07443d96f34367ca8311786604cb91e88873f1f
ぎょくて <gyokutech@gmail.com>
2023-10-24 23:34:27 +0900

    mod FDMiObjectActivator
    汎用化

--- Packages/tech.gyoku.fdmi.core/Script/Object/FDMiObjectActivator.cs
+++ Packages/tech.gyoku.fdmi.core/Script/Object/FDMiObjectActivator.cs
@@ -5,27 +5,16 @@ using VRC.SDKBase;
 using VRC.Udon;
 namespace tech.gyoku.FDMi.core
 {
-    public class FDMiObjectActivator : FDMiAttribute
+    public class FDMiObjectActivator : FDMiBehaviour
     {
-        [SerializeField] FDMiBool InZone, IsPilot;
-        [SerializeField] bool enableInZone, enableIsPilot;
-        [SerializeField] bool disableInZone, disableIsPilot;
+        [SerializeField] FDMiBool Boolean;
+        [SerializeField] bool onWhenDisable;
         void Start()
         {
-            if (enableInZone || disableInZone) InZone.subscribe(this, "OnChangeInZone");
-            if (enableIsPilot || disableIsPilot) IsPilot.subscribe(this, "OnChangeIsPilot");
-            OnChangeInZone();
-            OnChangeIsPilot();
+            Boolean.subscribe(this, "OnChange");
+            OnChange();
         }
-        public void OnChangeInZone()
-        {
-            if (enableInZone) gameObject.SetActive(InZone.data[0]);
-            if (disableInZone) gameObject.SetActive(!InZone.data[0]);
-        }
-        public void OnChangeIsPilot()
-        {
-            if (enableIsPilot) gameObject.SetActive(IsPilot.data[0]);
-            if (disableIsPilot) gameObject.SetActive(!IsPilot.data[0]);
-        }
+        public void OnChange() { gameObject.SetActive(Boolean.data[0] ^ onWhenDisable); }
+
     }
 }
