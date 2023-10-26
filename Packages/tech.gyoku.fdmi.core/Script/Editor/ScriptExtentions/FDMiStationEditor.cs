ce7204a1518748ca6aaa12aea418315beeda8aff
ぎょくて <gyokutech@gmail.com>
2023-10-26 15:07:26 +0900

    Create FDMiStationEditor.cs

---
+++ Packages/tech.gyoku.fdmi.core/Script/Editor/ScriptExtentions/FDMiStationEditor.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UdonSharp;
+using UdonSharpEditor;
+using UnityEditor;
+using UnityEngine;
+using tech.gyoku.FDMi.core;
+
+namespace tech.gyoku.FDMi.core.editor
+{
+    [CustomEditor(typeof(FDMiStation), true)]
+    public class FDMiStationEditor : FDMiAttributeEditor
+    {
+        public override void SetPropertyOption(Component tgt, SerializedProperty property, bool forceSetup)
+        {
+            base.SetPropertyOption(tgt, property, forceSetup);
+            if (property.name == nameof(FDMiStation.InSeatBehaviours))
+            {
+                if ((FDMiEditorUI.Button("Find") || forceSetup))
+                {
+                    UdonSharpBehaviour[] behaviours = FDMiEditorUI.FindChildrenComponents<UdonSharpBehaviour>(tgt).ToArray();
+                    FDMiEditorUI.SetObjectArrayProperty<UdonSharpBehaviour>(property, behaviours);
+
+                }
+            }
+        }
+    }
+}
