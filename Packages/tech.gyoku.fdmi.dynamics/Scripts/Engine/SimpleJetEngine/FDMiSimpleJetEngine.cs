8a44a847ac25f930fc37feedb936cb60717f8b44
ぎょくて <gyokutech@gmail.com>
2023-11-06 01:56:37 +0900

    SimpleJetEngine
    ・挙動の微調整

--- Packages/tech.gyoku.fdmi.dynamics/Scripts/Engine/SimpleJetEngine/FDMiSimpleJetEngine.cs
+++ Packages/tech.gyoku.fdmi.dynamics/Scripts/Engine/SimpleJetEngine/FDMiSimpleJetEngine.cs
@@ -16,7 +16,7 @@ namespace tech.gyoku.FDMi.dynamics
         [SerializeField] private float N2min = 0.6f, N2max = 1.1f, maxAirN2 = 0.4f, airN2Coef = 0.1f;
         [SerializeField] private float N1ReverserRatio = -0.5f, N2ReverserRatio = 1f;
         [SerializeField] private float ffIdle = 0.47f, ffMax = 3.145f, egtMax = 1100f;
-        [SerializeField] private float N1dt = 1f, N2dt = 1f, ffDt = 1f, egtDt = 2f;
+        [SerializeField] private float N1dt = 1f, N2dt = 1f, ffDt = 1f, egtDt = 2f, inputNoise = 0.02f;
         [SerializeField] AnimationCurve thrustMachMultiplier;
         [SerializeField] AnimationCurve thrustPressureMultiplier;
         float[] sw, airsw, input, rev, mach, pressure, ff, egt, sat, n1, n2;
@@ -38,19 +38,22 @@ namespace tech.gyoku.FDMi.dynamics
         void FixedUpdate()
         {
             if (!isInit) return;
-            // Fuel flow(kg/s)
-            ff[0] = Mathf.MoveTowards(ff[0], sw[0] * Mathf.Lerp(ffIdle, ffMax, input[0]), ffDt * Time.fixedDeltaTime);
-            // EGT calculate section
-            egt[0] = Mathf.MoveTowards(egt[0], Mathf.Lerp(sat[0], egtMax, ff[0] / ffMax), egtDt * Time.fixedDeltaTime);
-            egt[0] = Mathf.Max(egt[0], sat[0]);
+            float theta = Mathf.Sqrt(sat[0] / 288f) * Time.fixedDeltaTime;
+            float noise = 1 + Mathf.Lerp(inputNoise, -inputNoise, Mathf.PerlinNoise(Time.time, 0));
 
-            float theta = Mathf.Sqrt(sat[0] / 288f);
-            float N2tgt = Mathf.Max(maxAirN2 * airsw[0], sw[0] * Mathf.Lerp(N2min, N2max, input[0]));
-            n2[0] = Mathf.MoveTowards(n2[0], N2tgt, N2dt * Time.fixedDeltaTime * theta);
+            float N2tgt = sw[0] * Mathf.Lerp(N2min, N2max, input[0]);
+            n2[0] = Mathf.MoveTowards(n2[0], N2tgt * noise, N2dt * theta);
+            n2[0] = Mathf.Max(n2[0], Mathf.MoveTowards(n2[0], maxAirN2 * airsw[0] * noise, airN2Coef * theta));
 
             float dN1 = n2[0] * 2f - 1f - n1[0];
-            n1[0] += N1dt * dN1 * Time.fixedDeltaTime * theta;
+            n1[0] = Mathf.MoveTowards(n1[0], n1[0] + dN1 * noise, N1dt * theta);
             n1[0] = Mathf.Max(n1[0], 0f);
+
+            // Fuel flow(kg/s)
+            ff[0] = Mathf.MoveTowards(ff[0], sw[0] * Mathf.Lerp(ffIdle, ffMax, input[0]), ffDt * theta);
+            // EGT calculate section
+            egt[0] = Mathf.MoveTowards(egt[0], Mathf.Lerp(sat[0], egtMax, Mathf.Sqrt(ff[0] / ffMax) * noise), egtDt * theta);
+            egt[0] = Mathf.Max(egt[0], sat[0]);
 
             if (isOwner)
             {
