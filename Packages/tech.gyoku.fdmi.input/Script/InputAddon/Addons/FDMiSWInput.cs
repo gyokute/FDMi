d8ec3df027d818ebdad1d687fa940beedacbe8c3
ぎょくて <gyokutech@gmail.com>
2023-11-07 20:34:59 +0900

    FDMiSWInput
    ・FDMiInputのbool版

---
+++ Packages/tech.gyoku.fdmi.input/Script/InputAddon/Addons/FDMiSWInput.cs
@@ -0,0 +1,36 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using tech.gyoku.FDMi.core;
+
+namespace tech.gyoku.FDMi.input
+{
+
+    public class FDMiSWInput : FDMiInputAddon
+    {
+        [SerializeField] FDMiBool Output;
+        [SerializeField] AxisBehaviourType behaviourType;
+        [SerializeField] bool reg = true;
+
+        public override void OnCalled(KeyCode callKey, VRCPlayerApi.TrackingDataType trackType)
+        {
+            base.OnCalled(callKey, trackType);
+            if (behaviourType == AxisBehaviourType.alternate)
+            {
+                Output.set(!Output.Data);
+            }
+            if (behaviourType == AxisBehaviourType.momentum || behaviourType == AxisBehaviourType.force)
+            {
+                Output.set(reg);
+            }
+        }
+        public override void OnReleased()
+        {
+            base.OnReleased();
+            if (behaviourType == AxisBehaviourType.momentum)
+                Output.set(!reg);
+        }
+    }
+}
