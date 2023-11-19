474e584599b5b730555773949df63c0274d9cf5b
ぎょくて <gyokutech@gmail.com>
2023-11-19 18:25:26 +0900

    FDMiSoundController
    ・音（ジェット）

---
+++ Packages/tech.gyoku.fdmi.core/Script/Animation/FDMiSoundController.cs
@@ -0,0 +1,29 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace tech.gyoku.FDMi.core
+{
+    public class FDMiSoundController : FDMiBehaviour
+    {
+        [SerializeField] AudioSource sound;
+        [SerializeField] FDMiFloat SoundVolume, SoundPitch;
+        [SerializeField] AnimationCurve gainCurve, pitchCurve;
+        float[] vol, pitch;
+
+        void Start()
+        {
+            vol = SoundVolume.data;
+            pitch = SoundPitch.data;
+        }
+
+        void Update()
+        {
+            sound.volume = gainCurve.Evaluate(vol[0]);
+            sound.pitch = pitchCurve.Evaluate(pitch[0]);
+        }
+
+    }
+}
