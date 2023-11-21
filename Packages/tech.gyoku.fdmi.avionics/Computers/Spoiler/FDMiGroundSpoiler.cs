62feb76d4f7e2232bf50ebbf2b044ac26d738919
ぎょくて <gyokutech@gmail.com>
2023-11-22 00:07:35 +0900

    SpoilerControl
    ・スポイレロン
    ・グラウンドスポイラー

--- Packages/tech.gyoku.fdmi.avionics/Computers/Spoiler/FDMiGroundSpoiler.cs
+++ Packages/tech.gyoku.fdmi.avionics/Computers/Spoiler/FDMiGroundSpoiler.cs
@@ -3,11 +3,40 @@ using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
+using tech.gyoku.FDMi.core;
 
-public class FDMiGroundSpoiler : UdonSharpBehaviour
+namespace tech.gyoku.FDMi.avionics
 {
-    void Start()
+
+    public class FDMiGroundSpoiler : FDMiBehaviour
     {
-        
+        [SerializeField] FDMiFloat SpoilerLever, GroundSpoilerLever;
+        [SerializeField] FDMiBool[] IsGround;
+        [SerializeField] float SpoilerLeverMaxMove = 1.2f, MoveSpeed = 1f;
+        float[] gsLever, spoiler;
+        private bool armed = false;
+        void Start()
+        {
+            spoiler = SpoilerLever.data;
+            gsLever = GroundSpoilerLever.data;
+            GroundSpoilerLever.subscribe(this, nameof(OnChangeGSLever));
+            gameObject.SetActive(false);
+        }
+        int i;
+        void Update()
+        {
+            armed = false;
+            for (i = 0; i < IsGround.Length; i++)
+            {
+                armed = armed || IsGround[i].data[0];
+            }
+            SpoilerLever.Data = Mathf.MoveTowards(spoiler[0], armed ? SpoilerLeverMaxMove : 0, MoveSpeed * Time.deltaTime);
+            if (Mathf.Approximately(spoiler[0], SpoilerLeverMaxMove)) GroundSpoilerLever.Data = 0f;
+        }
+
+        public void OnChangeGSLever()
+        {
+            gameObject.SetActive(gsLever[0] > 0.75f);
+        }
     }
 }
---
+++ Packages/tech.gyoku.fdmi.avionics/Computers/Spoiler/FDMiSpoileron.cs
@@ -0,0 +1,39 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using tech.gyoku.FDMi.core;
+
+namespace tech.gyoku.FDMi.avionics
+{
+
+    public class FDMiSpoileron : FDMiBehaviour
+    {
+        [SerializeField] FDMiFloat SpoilerL, SpoilerR, SpoilerLever, RollOutput;
+        [SerializeField] float moveSpeed;
+        float[] lever, roll, sl, sr;
+
+        void Start()
+        {
+            lever = SpoilerLever.data;
+            roll = RollOutput.data;
+            sl = SpoilerL.data;
+            sr = SpoilerR.data;
+        }
+        float movedt;
+        void Update()
+        {
+            // GroundSpoiler Active
+            if (lever[0] > 1f)
+            {
+                SpoilerL.Data = lever[0];
+                SpoilerR.Data = lever[0];
+                return;
+            }
+            movedt = moveSpeed * Time.deltaTime;
+            SpoilerL.Data = Mathf.MoveTowards(sl[0], Mathf.Clamp01(lever[0] - roll[0]), movedt);
+            SpoilerR.Data = Mathf.MoveTowards(sr[0], Mathf.Clamp01(lever[0] + roll[0]), movedt);
+        }
+    }
+}
