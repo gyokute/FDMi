4f6a45ccb635f522fbc894eb3a907cf25a36e8de
ぎょくて <gyokutech@gmail.com>
2023-11-29 22:59:04 +0900

    GroundAltitude
    ・アルゴリズムを変更

--- Packages/tech.gyoku.fdmi.avionics/Computers/Reference/FDMiGroundAltitude.cs
+++ Packages/tech.gyoku.fdmi.avionics/Computers/Reference/FDMiGroundAltitude.cs
@@ -11,25 +11,18 @@ namespace tech.gyoku.FDMi.avionics
     public class FDMiGroundAltitude : FDMiBehaviour
     {
         [SerializeField] FDMiFloat GroundAltitude;
-        [SerializeField] FDMiQuaternion Rotation;
+        [SerializeField] Transform GroundReference;
 
         [SerializeField] float maxLength = 914.4f, offset;
         [SerializeField] LayerMask groundLayer;
-        Quaternion[] rot;
-        Vector3 down;
-
         RaycastHit[] hit = new RaycastHit[32];
-        void Start()
-        {
-            rot = Rotation.data;
-        }
+
         int i;
         float minimum;
         void Update()
         {
-            down = rot[0] * -Vector3.up;
             minimum = maxLength;
-            for (i = 0; i < Physics.RaycastNonAlloc(transform.position, down, hit, maxLength, groundLayer); i++)
+            for (i = 0; i < Physics.RaycastNonAlloc(transform.position, -GroundReference.up, hit, maxLength, groundLayer); i++)
                 minimum = Mathf.Min(minimum, hit[i].distance);
             GroundAltitude.Data = minimum + offset;
         }
