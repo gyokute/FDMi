cd5c9063cbbc76340e302d12921da8eba30fc67d
ぎょくて <gyokutech@gmail.com>
2023-12-03 01:56:06 +0900

    FDMiObjectActivateByValue

---
+++ Packages/tech.gyoku.fdmi.core/Script/Object/FDMiObjectActivateByValue.cs
@@ -0,0 +1,27 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+namespace tech.gyoku.FDMi.core
+{
+    public class FDMiObjectActivateByValue : FDMiBehaviour
+    {
+        [SerializeField] FDMiFloat Value;
+        [SerializeField] AnimationCurve turnOnCurve;
+        [SerializeField] private bool moveOnValueChange, moveOnUpdate;
+        void Start()
+        {
+            if (moveOnValueChange) Value.subscribe(this, "OnChange");
+            gameObject.SetActive(moveOnUpdate);
+        }
+        void Update()
+        {
+            OnChange();
+        }
+        public void OnChange()
+        {
+            gameObject.SetActive(turnOnCurve.Evaluate(Value.Data) > 0.5f);
+        }
+    }
+}
