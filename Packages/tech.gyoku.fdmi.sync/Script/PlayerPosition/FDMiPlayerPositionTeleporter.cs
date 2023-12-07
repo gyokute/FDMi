aca55710c595880aca03f0f00741ecff51789ea7
ぎょくて <gyokutech@gmail.com>
2023-12-07 21:34:04 +0900

    Teleporter

---
+++ Packages/tech.gyoku.fdmi.sync/Script/PlayerPosition/FDMiPlayerPositionTeleporter.cs
@@ -0,0 +1,18 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace tech.gyoku.FDMi.sync
+{
+    public class FDMiPlayerPositionTeleporter : UdonSharpBehaviour
+    {
+        public FDMiRelativePositionTrigger teleportTarget, teleportFrom;
+        public Transform teleportPosition;
+        public void ExecTeleport(){
+            if(teleportFrom) teleportFrom.ExitLocalPlayer();
+            if(teleportTarget) teleportTarget.TeleportLocalPlayer(teleportPosition);
+        }
+    }
+}
