022f1d0afe79ebba63d30449a8cd9ab236c5ee9a
ぎょくて <gyokutech@gmail.com>
2023-12-31 05:28:32 +0900

    ドップラー効果
    ・音のチューニングが詰まったので実装

--- Packages/tech.gyoku.fdmi.core/Script/Animation/FDMiSoundController.cs
+++ Packages/tech.gyoku.fdmi.core/Script/Animation/FDMiSoundController.cs
@@ -11,18 +11,34 @@ namespace tech.gyoku.FDMi.core
         [SerializeField] AudioSource sound;
         [SerializeField] FDMiFloat SoundVolume, SoundPitch;
         [SerializeField] AnimationCurve gainCurve, pitchCurve;
+        [SerializeField] bool useDopplerEffect;
         float[] vol, pitch;
+        VRCPlayerApi local;
 
         void Start()
         {
             vol = SoundVolume.data;
             pitch = SoundPitch.data;
         }
-
+        public override void OnPlayerJoined(VRCPlayerApi player)
+        {
+            if (player.isLocal) local = player;
+        }
+        Vector3 movingVec, prevPos;
+        float relativeSpeed, dopplerPitch = 1f;
         void Update()
         {
+            if (local != null && useDopplerEffect)
+            {
+                movingVec = (transform.position - local.GetPosition() - prevPos) / Time.deltaTime;
+                relativeSpeed = Mathf.Max(Vector3.Dot(movingVec, prevPos.normalized), -255f);
+                prevPos = transform.position - local.GetPosition();
+                dopplerPitch = Mathf.MoveTowards(dopplerPitch, 340f / (340f + relativeSpeed), Time.deltaTime);
+
+            }
             sound.volume = gainCurve.Evaluate(vol[0]);
-            sound.pitch = pitchCurve.Evaluate(pitch[0]);
+            sound.pitch = pitchCurve.Evaluate(pitch[0]) * dopplerPitch;
+
         }
 
     }
