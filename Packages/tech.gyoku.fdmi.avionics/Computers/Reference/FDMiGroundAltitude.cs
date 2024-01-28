f5773150a215432aa2d6167077150ad2109af305
ぎょくて <gyokutech@gmail.com>
2024-01-28 23:06:36 +0900

    chore: FDMiGroundAltitude
    * 導入時の障害排除

--- Packages/tech.gyoku.fdmi.avionics/Computers/Reference/FDMiGroundAltitude.cs
+++ Packages/tech.gyoku.fdmi.avionics/Computers/Reference/FDMiGroundAltitude.cs
@@ -11,18 +11,22 @@ namespace tech.gyoku.FDMi.avionics
     public class FDMiGroundAltitude : FDMiBehaviour
     {
         [SerializeField] FDMiFloat GroundAltitude;
-        [SerializeField] Transform GroundReference;
-
+        [SerializeField] FDMiQuaternion Rotation;
         [SerializeField] float maxLength = 914.4f, offset;
         [SerializeField] LayerMask groundLayer;
         RaycastHit[] hit = new RaycastHit[32];
 
         int i;
         float minimum;
+        Quaternion[] rot;
+        void Start()
+        {
+            rot = Rotation.data;
+        }
         void Update()
         {
             minimum = maxLength;
-            for (i = 0; i < Physics.RaycastNonAlloc(transform.position, -GroundReference.up, hit, maxLength, groundLayer); i++)
+            for (i = 0; i < Physics.RaycastNonAlloc(transform.position, Quaternion.Inverse(rot[0]) * Vector3.down, hit, maxLength, groundLayer); i++)
                 minimum = Mathf.Min(minimum, hit[i].distance);
             GroundAltitude.Data = minimum + offset;
         }
