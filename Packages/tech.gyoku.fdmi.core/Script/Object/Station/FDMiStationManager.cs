6bde27613e24bbbe411a7710ee50ffe4fbb10397
ぎょくて <gyokutech@gmail.com>
2024-02-05 22:42:31 +0900

    fix: FDMiStationManager
    * OnlyIsRootの依存性（既に意味なし）を排除

--- Packages/tech.gyoku.fdmi.core/Script/Object/Station/FDMiStationManager.cs
+++ Packages/tech.gyoku.fdmi.core/Script/Object/Station/FDMiStationManager.cs
@@ -8,7 +8,7 @@ namespace tech.gyoku.FDMi.core
 {
     public class FDMiStationManager : FDMiAttribute
     {
-        public Transform onlyIsRoot;
+        // public Transform onlyIsRoot;
         public FDMiStation[] stations;
         public FDMiBool IsPilot, InZone;
         void Start()
@@ -18,22 +18,15 @@ namespace tech.gyoku.FDMi.core
             InZone.subscribe(this, "OnChangeInZone");
         }
         #region Owner Transfer
+        public override void OnPlayerLeft(VRCPlayerApi player)
+        {
+            if (player.isLocal) TryDelegatePilot();
+        }
         public void OnChangeInZone()
         {
-            if (InZone.data[0] == true)
-            {
-                if (onlyIsRoot) transform.parent = onlyIsRoot;
-                transform.localPosition = Vector3.zero;
-                transform.localRotation = Quaternion.identity;
-            }
-            else
-            {
-                transform.parent = body.transform;
-                transform.localPosition = Vector3.zero;
-                transform.localRotation = Quaternion.identity;
-                TryDelegatePilot();
-            }
+            if (InZone.data[0] != true) TryDelegatePilot();
         }
+
         public void TryTakePilot(int pilotPriority)
         {
             for (int i = 0; i < stations.Length; i++)
