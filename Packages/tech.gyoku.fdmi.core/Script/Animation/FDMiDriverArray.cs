f8311a0a3d2afbb19b9ccf50e3356d85c43ad150
ぎょくて <gyokutech@gmail.com>
2024-02-08 00:50:25 +0900

    chore: Animation Driver Array
    * FDMiAnimationDriverArray: AnimationDriverをまとめて配列化し、一つのgameObjectに集約
    * FDMiTransformRotationDriverArray: FDMiTransformRotationDriverをまとめて配列化し、一つのgameObjectに集約

---
+++ Packages/tech.gyoku.fdmi.core/Script/Animation/FDMiDriverArray.cs
@@ -0,0 +1,144 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace tech.gyoku.FDMi.core
+{
+    public enum DriverUpdateMode { OnValueChange, OnUpdate }
+    public class FDMiDriverArray : FDMiBehaviour
+    {
+        public virtual void OnChange(int i) { }
+        public void OnChange0() { OnChange(0); }
+        public void OnChange1() { OnChange(1); }
+        public void OnChange2() { OnChange(2); }
+        public void OnChange3() { OnChange(3); }
+        public void OnChange4() { OnChange(4); }
+        public void OnChange5() { OnChange(5); }
+        public void OnChange6() { OnChange(6); }
+        public void OnChange7() { OnChange(7); }
+        public void OnChange8() { OnChange(8); }
+        public void OnChange9() { OnChange(9); }
+        public void OnChange10() { OnChange(10); }
+        public void OnChange11() { OnChange(11); }
+        public void OnChange12() { OnChange(12); }
+        public void OnChange13() { OnChange(13); }
+        public void OnChange14() { OnChange(14); }
+        public void OnChange15() { OnChange(15); }
+        public void OnChange16() { OnChange(16); }
+        public void OnChange17() { OnChange(17); }
+        public void OnChange18() { OnChange(18); }
+        public void OnChange19() { OnChange(19); }
+        public void OnChange20() { OnChange(20); }
+        public void OnChange21() { OnChange(21); }
+        public void OnChange22() { OnChange(22); }
+        public void OnChange23() { OnChange(23); }
+        public void OnChange24() { OnChange(24); }
+        public void OnChange25() { OnChange(25); }
+        public void OnChange26() { OnChange(26); }
+        public void OnChange27() { OnChange(27); }
+        public void OnChange28() { OnChange(28); }
+        public void OnChange29() { OnChange(29); }
+        public void OnChange30() { OnChange(30); }
+        public void OnChange31() { OnChange(31); }
+        public void OnChange32() { OnChange(32); }
+        public void OnChange33() { OnChange(33); }
+        public void OnChange34() { OnChange(34); }
+        public void OnChange35() { OnChange(35); }
+        public void OnChange36() { OnChange(36); }
+        public void OnChange37() { OnChange(37); }
+        public void OnChange38() { OnChange(38); }
+        public void OnChange39() { OnChange(39); }
+        public void OnChange40() { OnChange(40); }
+        public void OnChange41() { OnChange(41); }
+        public void OnChange42() { OnChange(42); }
+        public void OnChange43() { OnChange(43); }
+        public void OnChange44() { OnChange(44); }
+        public void OnChange45() { OnChange(45); }
+        public void OnChange46() { OnChange(46); }
+        public void OnChange47() { OnChange(47); }
+        public void OnChange48() { OnChange(48); }
+        public void OnChange49() { OnChange(49); }
+        public void OnChange50() { OnChange(50); }
+        public void OnChange51() { OnChange(51); }
+        public void OnChange52() { OnChange(52); }
+        public void OnChange53() { OnChange(53); }
+        public void OnChange54() { OnChange(54); }
+        public void OnChange55() { OnChange(55); }
+        public void OnChange56() { OnChange(56); }
+        public void OnChange57() { OnChange(57); }
+        public void OnChange58() { OnChange(58); }
+        public void OnChange59() { OnChange(59); }
+        public void OnChange60() { OnChange(60); }
+        public void OnChange61() { OnChange(61); }
+        public void OnChange62() { OnChange(62); }
+        public void OnChange63() { OnChange(63); }
+        public void OnChange64() { OnChange(64); }
+        public void OnChange65() { OnChange(65); }
+        public void OnChange66() { OnChange(66); }
+        public void OnChange67() { OnChange(67); }
+        public void OnChange68() { OnChange(68); }
+        public void OnChange69() { OnChange(69); }
+        public void OnChange70() { OnChange(70); }
+        public void OnChange71() { OnChange(71); }
+        public void OnChange72() { OnChange(72); }
+        public void OnChange73() { OnChange(73); }
+        public void OnChange74() { OnChange(74); }
+        public void OnChange75() { OnChange(75); }
+        public void OnChange76() { OnChange(76); }
+        public void OnChange77() { OnChange(77); }
+        public void OnChange78() { OnChange(78); }
+        public void OnChange79() { OnChange(79); }
+        public void OnChange80() { OnChange(80); }
+        public void OnChange81() { OnChange(81); }
+        public void OnChange82() { OnChange(82); }
+        public void OnChange83() { OnChange(83); }
+        public void OnChange84() { OnChange(84); }
+        public void OnChange85() { OnChange(85); }
+        public void OnChange86() { OnChange(86); }
+        public void OnChange87() { OnChange(87); }
+        public void OnChange88() { OnChange(88); }
+        public void OnChange89() { OnChange(89); }
+        public void OnChange90() { OnChange(90); }
+        public void OnChange91() { OnChange(91); }
+        public void OnChange92() { OnChange(92); }
+        public void OnChange93() { OnChange(93); }
+        public void OnChange94() { OnChange(94); }
+        public void OnChange95() { OnChange(95); }
+        public void OnChange96() { OnChange(96); }
+        public void OnChange97() { OnChange(97); }
+        public void OnChange98() { OnChange(98); }
+        public void OnChange99() { OnChange(99); }
+        public void OnChange100() { OnChange(100); }
+        public void OnChange101() { OnChange(101); }
+        public void OnChange102() { OnChange(102); }
+        public void OnChange103() { OnChange(103); }
+        public void OnChange104() { OnChange(104); }
+        public void OnChange105() { OnChange(105); }
+        public void OnChange106() { OnChange(106); }
+        public void OnChange107() { OnChange(107); }
+        public void OnChange108() { OnChange(108); }
+        public void OnChange109() { OnChange(109); }
+        public void OnChange110() { OnChange(110); }
+        public void OnChange111() { OnChange(111); }
+        public void OnChange112() { OnChange(112); }
+        public void OnChange113() { OnChange(113); }
+        public void OnChange114() { OnChange(114); }
+        public void OnChange115() { OnChange(115); }
+        public void OnChange116() { OnChange(116); }
+        public void OnChange117() { OnChange(117); }
+        public void OnChange118() { OnChange(118); }
+        public void OnChange119() { OnChange(119); }
+        public void OnChange120() { OnChange(120); }
+        public void OnChange121() { OnChange(121); }
+        public void OnChange122() { OnChange(122); }
+        public void OnChange123() { OnChange(123); }
+        public void OnChange124() { OnChange(124); }
+        public void OnChange125() { OnChange(125); }
+        public void OnChange126() { OnChange(126); }
+        public void OnChange127() { OnChange(127); }
+        public void OnChange128() { OnChange(128); }
+
+    }
+}
---
+++ Packages/tech.gyoku.fdmi.core/Script/Animation/FDMiTransformRotationDriverArray.cs
@@ -0,0 +1,46 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace tech.gyoku.FDMi.core
+{
+    public class FDMiTransformRotationDriverArray : FDMiDriverArray
+    {
+        [SerializeField] FDMiFloat[] Input;
+        [SerializeField] Transform[] rotateTransform;
+        [SerializeField] Vector3[] rotateAxis;
+        [SerializeField] AnimationCurve[] multiplier;
+        [SerializeField] private float[] repeatValue;
+        [SerializeField] private DriverUpdateMode updateMode;
+        private Vector3[] initial;
+        void Start()
+        {
+            initial = new Vector3[Input.Length];
+            for (int i = 0; i < Mathf.Min(Input.Length, 128); i++)
+            {
+                initial[i] = rotateTransform[i].localEulerAngles;
+                if (updateMode == DriverUpdateMode.OnValueChange)
+                    Input[i].subscribe(this, "OnChange" + i.ToString());
+            }
+            gameObject.SetActive(updateMode == DriverUpdateMode.OnUpdate);
+        }
+
+        void Update()
+        {
+            if (updateMode != DriverUpdateMode.OnUpdate) return;
+            for (int i = 0; i < Mathf.Min(Input.Length, 128); i++)
+            {
+                OnChange(i);
+            }
+        }
+
+        float dat;
+        public override void OnChange(int i)
+        {
+            dat = Mathf.Approximately(repeatValue[i], 0) ? Input[i].Data : Mathf.Repeat(Input[i].Data, repeatValue[i]);
+            rotateTransform[i].localEulerAngles = initial[i] + multiplier[i].Evaluate(dat) * rotateAxis[i];
+        }
+    }
+}
