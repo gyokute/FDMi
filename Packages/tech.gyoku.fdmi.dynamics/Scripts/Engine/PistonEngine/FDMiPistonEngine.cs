3afd564378ef660412cc15bbb5408c666e79e96e
ぎょくて <gyokutech@gmail.com>
2024-02-18 17:31:23 +0900

    feature: レシプロエンジン

---
+++ Packages/tech.gyoku.fdmi.dynamics/Scripts/Engine/PistonEngine/FDMiPistonEngine.cs
@@ -0,0 +1,70 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+using tech.gyoku.FDMi.core;
+
+namespace tech.gyoku.FDMi.dynamics
+{
+    public class FDMiPistonEngine : FDMiBehaviour
+    {
+        [SerializeField] FDMiFloat Throttle, Mixture, Starter, Magneto;
+        [SerializeField] FDMiFloat PowerTrainRPM, OutputTorque;
+        [SerializeField] FDMiFloat Rho, IntakePressure;
+        [SerializeField] AnimationCurve torqueMap;
+        [SerializeField] AnimationCurve mixtureMap;
+        [SerializeField] float loss, inertia, starterMaxRPM = 500f, idleThrottle = 0.01f, starterTorque = 50f, starterThreshold = 0.9f, magnetoThreshold = 0.05f;
+        [Header("エンジン排気量[m^3]. Lを1000分の1にして代入")]
+        [SerializeField] float displacement = 0.0054f;
+        [Header("スロート断面積[m^2]")]
+        [SerializeField] float throatArea = 0.001f;
+        [Header("エンジン空気流量[m^3].")]
+        [SerializeField] float Q;
+        [Header("マニホールド圧[Pa]")]
+        [SerializeField] float manPressure;
+        [Header("海上におけるミクスチャ最大のときの空燃比.リッチ(1/10)程度に設定する事！")]
+        [SerializeField] float maxMixtureAFR = 0.1f;
+        [Header("現在の燃料流量[kg/s]")]
+        [SerializeField] float currentFuelFlow;
+        [Header("混合比(空気/燃料)")]
+        [SerializeField] float mixtureRatio;
+
+
+        float[] throttle, mixture, rpm, output, starter, mag;
+        float[] rho, p0;
+
+        void Start()
+        {
+            throttle = Throttle.data;
+            mixture = Mixture.data;
+            rpm = PowerTrainRPM.data;
+            output = OutputTorque.data;
+            starter = Starter.data;
+            mag = Magneto.data;
+            rho = Rho.data;
+            p0 = IntakePressure.data;
+        }
+        [SerializeField] float engineTorque, pureEngineTorque;
+        float currentThrottle, throatAirSpeed;
+        void FixedUpdate()
+        {
+            currentThrottle = Mathf.Lerp(idleThrottle, 1f, throttle[0]);
+
+            Q = displacement * rpm[0] / 120 * currentThrottle;
+            throatAirSpeed = Mathf.Lerp(Q / throatArea, throatAirSpeed, 0.5f);
+            manPressure = p0[0] + rho[0] * 0.5f * (1 - 1 / (currentThrottle * currentThrottle)) * throatAirSpeed * throatAirSpeed;
+            manPressure *= 0.0002953f; //debug
+
+            currentFuelFlow = mixture[0] * Q * maxMixtureAFR;
+            mixtureRatio = currentFuelFlow / Mathf.Max(rho[0] * Q, 0.0000001f);
+            currentFuelFlow *= 7936.641f; //debug
+
+            pureEngineTorque = torqueMap.Evaluate(rpm[0]) * mixtureMap.Evaluate(mixtureRatio) * currentThrottle * ((mag[0] - magnetoThreshold) > 0 ? 1 : 0) - loss * rpm[0];
+            engineTorque = pureEngineTorque + output[0];
+            engineTorque += starterTorque * ((starter[0] - starterThreshold) > 0 ? (1 - rpm[0] / starterMaxRPM) : 0);
+            PowerTrainRPM.Data = rpm[0] + engineTorque / inertia;
+        }
+    }
+}
---
+++ Packages/tech.gyoku.fdmi.dynamics/Scripts/Engine/Propeller/FDMiFixedPitchPropeller.cs
@@ -0,0 +1,66 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using tech.gyoku.FDMi.core;
+
+namespace tech.gyoku.FDMi.dynamics
+{
+    public class FDMiFixedPitchPropeller : FDMiAttribute
+    {
+        public AnimationCurve Cl_Alpha;
+        public AnimationCurve Cd_Alpha;
+        public FDMiVector3 AirSpeed;
+        public FDMiFloat Rho;
+        public FDMiFloat PowerTrainRPM;
+        public FDMiFloat OutputTorque;
+        public int bladeNum = 2;
+        public float bladeRadius = 1.0f;
+        public float bladeChord = 0.2f;
+        public float bladePitch = 12.0f;
+        public float gearRatio = 10f;
+
+        private Vector3[] airSpeed;
+        private float[] rho, rpm, torqueOut;
+        float r;
+        void Start()
+        {
+            airSpeed = AirSpeed.data;
+            rho = Rho.data;
+            rpm = PowerTrainRPM.data;
+            torqueOut = OutputTorque.data;
+            r = bladeRadius * 0.7f;
+        }
+
+        private float j;
+        private float thrust, torque;
+        public float n, alpha, phi;
+        public Vector3 Vr, LiftVec, Force;
+        void FixedUpdate()
+        {
+            n = rpm[0] / gearRatio / 60;
+            Vr.y = 0f;
+            Vr.z = Vector3.Dot(airSpeed[0], transform.forward);
+            Vr.x = r * 6.2831853f * n;
+            phi = Vector3.SignedAngle(Vr, Vector3.right, Vector3.up);
+            alpha = bladePitch - Vector3.SignedAngle(Vr, Vector3.right, Vector3.up);
+
+            LiftVec.z = Vr.x;
+            LiftVec.x = -Vr.z;
+            LiftVec = LiftVec.normalized;
+            // Lift
+            Force = Cl_Alpha.Evaluate(alpha) * LiftVec;
+            // drag
+            Force -= Cd_Alpha.Evaluate(alpha) * Vr.normalized;
+            Force *= bladeNum * 0.5f * rho[0] * Vr.magnitude * Vr.magnitude * bladeChord * bladeRadius;
+            // thrust output
+            thrust = Vector3.Dot(Force, Vector3.forward);
+            body.AddRelativeForce(Vector3.Project(Force, Vector3.forward));
+            // Torque output
+            torque = Vector3.Dot(Force, Vector3.right) * gearRatio;
+            torqueOut[0] = Mathf.Lerp(torqueOut[0], torque, 0.5f);
+        }
+
+    }
+}
