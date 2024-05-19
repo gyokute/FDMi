a60b0c3a9b6d091378bc2e6a5722700b45e2eb21
ぎょくて <gyokutech@gmail.com>
2024-05-20 00:00:29 +0900

    feat(core): FDMiObjectActivate
    - FDMiDataBus対応度を高めるため、既存のFDMiObjectActivatorと同様の働きをする以下を追加
      - FDMiObjectActivateInZone
      - FDMiObjectActivateIsPilot

---
+++ Packages/tech.gyoku.fdmi.core/Runtime/Object/FDMiObjectActivateInZone.cs
@@ -0,0 +1,20 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace tech.gyoku.FDMi.core
+{
+    public class FDMiObjectActivateInZone : FDMiBehaviour
+    {
+        public FDMiBool InZone;
+        public bool onWhenDisable;
+        void Start()
+        {
+            InZone.subscribe(this, "OnChange");
+            OnChange();
+        }
+        public void OnChange() { gameObject.SetActive(InZone.data[0] ^ onWhenDisable); }
+    }
+}
---
+++ Packages/tech.gyoku.fdmi.core/Runtime/Object/FDMiObjectActivateIsPilot.cs
@@ -0,0 +1,20 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace tech.gyoku.FDMi.core
+{
+    public class FDMiObjectActivateIsPilot : FDMiBehaviour
+    {
+        public FDMiBool IsPilot;
+        public bool onWhenDisable;
+        void Start()
+        {
+            IsPilot.subscribe(this, "OnChange");
+            OnChange();
+        }
+        public void OnChange() { gameObject.SetActive(IsPilot.data[0] ^ onWhenDisable); }
+    }
+}
