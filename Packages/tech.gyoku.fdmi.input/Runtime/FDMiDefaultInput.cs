a38d09a55b0405a9cd8c2f5c340ffa51965bfb8f
ぎょくて <gyokutech@gmail.com>
2024-07-16 23:28:49 +0900

    fix(input): Default Input
    - FDMiDefaultInput now works correctly.

--- Packages/tech.gyoku.fdmi.input/Runtime/FDMiDefaultInput.cs
+++ Packages/tech.gyoku.fdmi.input/Runtime/FDMiDefaultInput.cs
@@ -3,19 +3,30 @@ using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
+using tech.gyoku.FDMi.core;
+
 namespace tech.gyoku.FDMi.input
 {
     public class FDMiDefaultInput : FDMiInputPage
     {
+        public FDMiBool IsPilot;
         public FDMiFingerTrackerType fingerTrackerType;
 
-        // void Update()
-        // {
-        //     if (!inputManager) return;
-        //     if (inputManager.defaultGrabObject[(int)fingerTrackerType] == this) return;
-        //     inputManager.defaultGrabObject[(int)fingerTrackerType] = this;
-        //     inputManager.fingerTrackers[(int)fingerTrackerType].targetInput = this;
-        // }
+        void Start()
+        {
+            IsPilot.subscribe(this, "OnChangeInZone");
+        }
+        public void OnChangeInZone()
+        {
+            foreach (var fingerTracker in inputManager.fingerTrackers)
+            {
+                if (fingerTracker.fingerType == fingerTrackerType)
+                {
+                    fingerTracker.setDefaultPage(IsPilot.Data ? this : null);
+                    return;
+                }
+            }
+        }
 
     }
 }
