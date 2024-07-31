ad16c3dfedb8401a7ebbadc26f1f48faa47f55fa
ぎょくて <gyokutech@gmail.com>
2024-08-01 01:53:20 +0900

    feat(avionics) TurnCordinator (wip)
    - 滑り計を追加
    - turn cordinatorはwip

---
+++ Packages/tech.gyoku.fdmi.avionics/Runtime/Computers/Reference/FDMiTurnCordinator.cs
@@ -0,0 +1,40 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using tech.gyoku.FDMi.core;
+
+namespace tech.gyoku.FDMi.avionics
+{
+    public class FDMiTurnCordinator : FDMiAttribute
+    {
+        public FDMiFloat Slip;
+        public FDMiFloat TurnRate;
+        public FDMiQuaternion Rotation;
+        public float gravity = 9.78f;
+        [SerializeField] float cutoffFrequency = 0.1f;
+        Quaternion[] rot;
+        private float omega;
+        void Start()
+        {
+            rot = Rotation.data;
+            omega = 1 / cutoffFrequency;
+        }
+
+        Vector3 centrifugalAcc, prevVelocity;
+        void Update()
+        {
+            // Turn Cordinator
+            // Vector3 turnRate = Quaternion.Inverse(rot[0]) * body.angularVelocity;
+            // TurnRate.Data = turnRate.x;
+
+            // Ball
+            centrifugalAcc = Vector3.Lerp(centrifugalAcc, (body.velocity - prevVelocity) / Time.deltaTime, Time.deltaTime * omega);
+            Vector3 a = Vector3.ProjectOnPlane(centrifugalAcc + gravity * (Quaternion.Inverse(rot[0]) * Vector3.down), Vector3.forward);
+            Slip.Data = Vector3.SignedAngle(Vector3.down, a, Vector3.forward);
+            prevVelocity = body.velocity;
+
+        }
+    }
+}
+
