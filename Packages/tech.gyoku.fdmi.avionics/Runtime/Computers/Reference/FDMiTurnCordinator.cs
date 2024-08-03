89f6b499e90f6e5e7e21baa81eed53519ea64781
ぎょくて <gyokutech@gmail.com>
2024-08-04 04:16:04 +0900

    chore: devide inclinometer from TurnCordinator
    - devide ball from turncordinator

--- Packages/tech.gyoku.fdmi.avionics/Runtime/Computers/Reference/FDMiTurnCordinator.cs
+++ Packages/tech.gyoku.fdmi.avionics/Runtime/Computers/Reference/FDMiTurnCordinator.cs
@@ -8,33 +8,23 @@ namespace tech.gyoku.FDMi.avionics
 {
     public class FDMiTurnCordinator : FDMiAttribute
     {
-        public FDMiFloat Slip;
         public FDMiFloat TurnRate;
         public FDMiQuaternion Rotation;
-        public float gravity = 9.78f;
-        [SerializeField] float cutoffFrequency = 0.1f;
+        public float rollRateMultiplier = 0.1f;
         Quaternion[] rot;
-        private float omega;
         void Start()
         {
             rot = Rotation.data;
-            omega = 1 / cutoffFrequency;
         }
 
         Vector3 centrifugalAcc, prevVelocity;
         void Update()
         {
             // Turn Cordinator
-            // Vector3 turnRate = Quaternion.Inverse(rot[0]) * body.angularVelocity;
-            // TurnRate.Data = turnRate.x;
+            float yawRate = (Quaternion.Inverse(rot[0]) * body.angularVelocity).y;
+            float rollRate = -body.angularVelocity.z;
 
-            // Ball
-            centrifugalAcc = Vector3.Lerp(centrifugalAcc, (body.velocity - prevVelocity) / Time.deltaTime, Time.deltaTime * omega);
-            Vector3 a = Vector3.ProjectOnPlane(centrifugalAcc + gravity * (Quaternion.Inverse(rot[0]) * Vector3.down), Vector3.forward);
-            Slip.Data = Vector3.SignedAngle(Vector3.down, a, Vector3.forward);
-            prevVelocity = body.velocity;
-
+            TurnRate.Data = Mathf.Rad2Deg * (rollRate * rollRateMultiplier + yawRate);
         }
     }
 }
-
