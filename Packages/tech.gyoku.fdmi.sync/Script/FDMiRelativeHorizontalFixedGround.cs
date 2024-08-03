37ba60df4c4afa480ec258b8dec03419fb34bf5f
ぎょくて <gyokutech@gmail.com>
2024-08-04 04:14:42 +0900

    fix: FDMiRelativeHorizontalFixedGround
    - Exception対策

--- Packages/tech.gyoku.fdmi.sync/Script/FDMiRelativeHorizontalFixedGround.cs
+++ Packages/tech.gyoku.fdmi.sync/Script/FDMiRelativeHorizontalFixedGround.cs
@@ -22,8 +22,8 @@ namespace tech.gyoku.FDMi.sync
                 diff -= rootRefPoint._position;
                 dir = Quaternion.Inverse(rootRefPoint._rotation);
             }
-            diff += 1000f * (kmDiff - syncManager.localPlayerPosition._kmPosition);
-            // diff += 1000f * kmDiff;
+            if (syncManager.localPlayerPosition) diff -= 1000f * syncManager.localPlayerPosition._kmPosition;
+            diff += 1000f * kmDiff;
             diff.x = 0f;
             diff.z = 0f;
             return dir * diff;
