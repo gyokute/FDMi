8622d758f30f03ffd254a4f6ccc1faea18dd2459
ぎょくて <gyokutech@gmail.com>
2024-08-25 15:18:18 +0900

    feat(sync): add curring distance to FDMiRelativeGroundSync

--- Packages/tech.gyoku.fdmi.sync/Script/FDMiRelativeGroundSync.cs
+++ Packages/tech.gyoku.fdmi.sync/Script/FDMiRelativeGroundSync.cs
@@ -9,6 +9,7 @@ namespace tech.gyoku.FDMi.sync
 {
     public class FDMiRelativeGroundSync : FDMiReferencePoint
     {
+        public float disableKmPos = 9999f;
         void Start()
         {
             setPosition(transform.position);
@@ -16,7 +17,39 @@ namespace tech.gyoku.FDMi.sync
         }
         public void LateUpdate()
         {
-            transform.SetPositionAndRotation(getViewPosition(), getViewRotation());
+            Vector3 relativePos = getViewPosition();
+            transform.SetPositionAndRotation(relativePos, getViewRotation());
+            if (relativePos.magnitude * 0.001f > disableKmPos + 1)
+            {
+                gameObject.SetActive(false);
+                SendCustomEventDelayedSeconds("UpdatePosWhenDisable", 1f);
+            }
+        }
+        public void UpdatePosWhenDisable()
+        {
+            Vector3 relativePos = getViewPosition();
+            if (relativePos.magnitude * 0.001f < disableKmPos - 1)
+            {
+                gameObject.SetActive(true);
+            }
+            else
+            {
+                SendCustomEventDelayedSeconds("UpdatePosWhenDisable", 1f);
+            }
+        }
+        public override void windupPositionAndRotation()
+        {
+            base.windupPositionAndRotation();
+            Vector3 relativePos = getViewPosition();
+            if (relativePos.magnitude * 0.001f < disableKmPos - 1)
+            {
+                gameObject.SetActive(true);
+            }
+            else
+            {
+                gameObject.SetActive(false);
+                SendCustomEventDelayedSeconds("UpdatePosWhenDisable", 1f);
+            }
         }
     }
 }
