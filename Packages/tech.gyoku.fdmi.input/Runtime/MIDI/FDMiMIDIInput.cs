b0e9777afd8cfbaebeb5dbbcfc9b3f4696ab8b29
ぎょくて <gyokutech@gmail.com>
2024-10-31 00:57:02 +0900

    feat(Input): MIDI Controller(WIP)

---
+++ Packages/tech.gyoku.fdmi.input/Runtime/MIDI/FDMiMIDIInput.cs
@@ -0,0 +1,20 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using tech.gyoku.FDMi.core;
+
+namespace tech.gyoku.FDMi.input
+{
+    public class FDMiMIDIInput : FDMiBehaviour
+    {
+        public FDMIMIDIInputConfigulation[] configs;
+        public int[] midiConfigIndex = new int[2048];
+        public override void MidiControlChange(int channel, int number, int value)
+        {
+            int configIndex = midiConfigIndex[channel * 128 + number];
+            configs[configIndex].onChange(value);
+        }
+    }
+}
