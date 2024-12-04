b4442727db9de53d8c7be7ff0a4fb3d1e5af4fd4
ぎょくて <gyokutech@gmail.com>
2024-12-04 20:17:09 +0900

    feat: FDMiAttitude magnetic deinclination
    - HDGに磁気偏角を考慮するよう変更

--- Packages/tech.gyoku.fdmi.avionics/Runtime/Computers/Reference/FDMiAttitude.cs
+++ Packages/tech.gyoku.fdmi.avionics/Runtime/Computers/Reference/FDMiAttitude.cs
@@ -7,14 +7,17 @@ using tech.gyoku.FDMi.core;
 
 namespace tech.gyoku.FDMi.avionics
 {
-    public class FDMiAttitude : FDMiAttribute
+    public class FDMiAttitude : FDMiBehaviour
     {
         public FDMiFloat Pitch, Roll, HDG;
         public FDMiQuaternion Rotation;
+        public FDMiFloat MagneticDeclination;
         Quaternion[] rot;
+        float[] mag;
         void Start()
         {
             rot = Rotation.data;
+            mag = MagneticDeclination.data;
         }
         Vector3 fwd, up, right;
         void Update()
@@ -22,7 +25,7 @@ namespace tech.gyoku.FDMi.avionics
             fwd = rot[0] * Vector3.forward;
             right = rot[0] * Vector3.right;
             up = rot[0] * Vector3.up;
-            HDG.Data = Mathf.Repeat(Vector3.SignedAngle(Vector3.forward, Vector3.ProjectOnPlane(fwd, Vector3.up), Vector3.up), 360f);
+            HDG.Data = Mathf.Repeat(Vector3.SignedAngle(Vector3.forward, Vector3.ProjectOnPlane(fwd, Vector3.up), Vector3.up) + mag[0], 360f);
             Pitch.Data = -Vector3.SignedAngle(Vector3.Cross(right, Vector3.up), fwd, right);
             Roll.Data = -Vector3.SignedAngle(Vector3.Cross(Vector3.up, fwd), right, fwd);
         }
