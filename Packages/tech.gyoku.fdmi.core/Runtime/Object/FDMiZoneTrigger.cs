db68d5ca28af1e429af8e0fef0c6a2978e92e72d
ぎょくて <gyokutech@gmail.com>
2024-12-22 16:51:52 +0900

    feat: FDMiZoneTrigger
    -単純な領域検知を追加

---
+++ Packages/tech.gyoku.fdmi.core/Runtime/Object/FDMiZoneTrigger.cs
@@ -0,0 +1,24 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace tech.gyoku.FDMi.core
+{
+    public class FDMiZoneTrigger : FDMiBehaviour
+    {
+        public FDMiBool Boolean;
+        [SerializeField] bool detectEnter = true, detectExit = true;
+
+        public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+        {
+            if (player.isLocal && detectEnter) Boolean.Data = true;
+        }
+
+        public override void OnPlayerTriggerExit(VRCPlayerApi player)
+        {
+            if (player.isLocal && detectExit) Boolean.Data = false;
+        }
+    }
+}
