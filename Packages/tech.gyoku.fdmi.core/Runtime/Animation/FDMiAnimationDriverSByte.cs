a0064658b894c3926fc154c5e9d5822f1364a8af
ぎょくて <gyokutech@gmail.com>
2026-01-01 02:22:46 +0900

    chore: SByte attributes

---
+++ Packages/tech.gyoku.fdmi.core/Runtime/Animation/FDMiAnimationDriverSByte.cs
@@ -0,0 +1,27 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+namespace tech.gyoku.FDMi.core
+{
+    public class FDMiAnimationDriverSByte : FDMiBehaviour
+    {
+        public FDMiSByte Input;
+        private sbyte[] input = new sbyte[1];
+        [SerializeField] private Animator animator;
+        [SerializeField] private string paramator;
+        [SerializeField] private AnimationCurve outputValue;
+        void Start()
+        {
+            input = Input.data;
+            Input.subscribe(this, "OnChange");
+        }
+
+        public void OnChange()
+        {
+            animator.SetFloat(paramator, outputValue.Evaluate(input[0]));
+        }
+
+    }
+}
