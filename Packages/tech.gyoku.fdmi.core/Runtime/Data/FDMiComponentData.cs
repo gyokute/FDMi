d643efa19877301b29bb1ee9467911addb1c1d94
ぎょくて <gyokutech@gmail.com>
2026-03-31 01:14:50 +0900

    feat: add FDMiComponentData

---
+++ Packages/tech.gyoku.fdmi.core/Runtime/Data/FDMiComponentData.cs
@@ -0,0 +1,25 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace tech.gyoku.FDMi.core
+{
+    public class FDMiComponentData : FDMiData
+    {
+        public Component[] data = new Component[1];
+        public Component Data
+        {
+            get => data[0];
+            set => set(value);
+        }
+        
+        public virtual void set(Component src)
+        { 
+            data[0] = src; 
+            trigger();
+        }
+
+    }
+}
