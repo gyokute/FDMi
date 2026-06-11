96e6e8b1881b5ec50033bdff6efe9ae9bdc69050
ぎょくて <gyokutech@gmail.com>
2026-06-12 01:06:50 +0900

    feat: add FDMiString

---
+++ Packages/tech.gyoku.fdmi.core/Runtime/Data/FDMiString.cs
@@ -0,0 +1,23 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace FDMi.core
+{
+    public class FDMiString : FDMiData
+    {
+        public string[] data = new string[1];
+        public string Data
+        {
+            get => data[0];
+            set => set(value);
+        }
+
+        public virtual void set(string src)
+        {
+            data[0] = src;
+            TriggerCallbacks();
+        }
+    }
+}
