b2c21d08484eb486fc12e54efff77c9bed29c71f
ぎょくて <gyokutech@gmail.com>
2026-06-12 01:04:51 +0900

    chore: remove commentout

--- Packages/tech.gyoku.fdmi.core/Runtime/Data/FDMiData.cs
+++ Packages/tech.gyoku.fdmi.core/Runtime/Data/FDMiData.cs
@@ -22,37 +22,5 @@ namespace FDMi.core
                 if (callbackBehaviour[i])
                     callbackBehaviour[i].SendCustomEvent(callbackFunction[i]);
         }
-
-#if UNITY_EDITOR && !COMPILER_UDONSHARP
-        /// <summary>
-        /// コールバックを登録する。
-        /// コールバックは、UdonSharpBehaviourと呼び出す関数の名前の組み合わせで登録される。
-        /// </summary>
-        /// <param name="behaviour">コールバックを呼び出すUdonSharpBehaviour</param>
-        /// <param name="functionName">呼び出す関数の名前</param>
-        public void Subscribe(UdonSharpBehaviour behaviour, string functionName)
-        {
-            // callbackBehaviourに配列要素を追加し、behaviourを格納する。
-            // callbackFunctionに配列要素を追加し、functionNameを格納する。
-            callbackBehaviour = callbackBehaviour.Append(behaviour).ToArray();
-            callbackFunction = callbackFunction.Append(functionName).ToArray();
-        }
-
-        /// <summary>
-        /// コールバックの登録を解除する。
-        /// behaviourとfunctionNameの組み合わせが一致するコールバックをcallbackBehaviourとcallbackFunctionから削除する。
-        /// 削除後、callbackBehaviourとcallbackFunctionの配列サイズを1減らす。
-        /// </summary>
-        /// <param name="behaviour">コールバックを呼び出すUdonSharpBehaviour</param>
-        /// <param name="functionName">呼び出す関数の名前</param>
-        public void Unsubscribe(UdonSharpBehaviour behaviour, string functionName)
-        {
-            var list = callbackBehaviour
-                .Select((b, i) => new { Behaviour = b, Function = callbackFunction[i] })
-                .Where(x => !(x.Behaviour == behaviour && x.Function == functionName));
-            callbackBehaviour = list.Select(x => x.Behaviour).ToArray();
-            callbackFunction = list.Select(x => x.Function).ToArray();
-        }
-#endif
     }
 }
