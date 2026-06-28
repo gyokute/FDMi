fa62ed6405668180b5ac2539818f3ca937d37edd
ぎょくて <gyokutech@gmail.com>
2026-06-28 09:30:42 +0900

    feat: add FDMiTransformRef

---
+++ Packages/tech.gyoku.fdmi.core/Runtime/Data/FDMiTransformRef.cs
@@ -0,0 +1,23 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace FDMi.core
+{
+    public class FDMiTransformRef : FDMiData
+    {
+        public Transform[] data = new Transform[1];
+        public Transform Data
+        {
+            get => data[0];
+            set => set(value);
+        }
+
+        public virtual void set(Transform src)
+        {
+            data[0] = src;
+            TriggerCallbacks();
+        }
+    }
+}
